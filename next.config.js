262059fbf057f261672d0c91e1951f2a263014b1
minjigang652-dotcom <minjigang652@gmail.com>
2026-09-23 09:59:17 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const repo = 'me-love-report';
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  images: { unoptimized: true },
+  trailingSlash: true,
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
 };
-
 module.exports = nextConfig;
